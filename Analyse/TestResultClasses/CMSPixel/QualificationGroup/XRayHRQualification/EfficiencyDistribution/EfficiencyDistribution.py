25210f3dc35436784dc311ca40ab8b0ddcb1ed02
Jamie Antonelli <lantone@gmail.com>
2016-01-29 16:49:16 -0600

    fixed 1D distributions that broke when updating the 2D summary plots

--- Analyse/TestResultClasses/CMSPixel/QualificationGroup/XRayHRQualification/EfficiencyDistribution/EfficiencyDistribution.py
+++ Analyse/TestResultClasses/CMSPixel/QualificationGroup/XRayHRQualification/EfficiencyDistribution/EfficiencyDistribution.py
@@ -8,15 +8,34 @@ class TestResult(AbstractClasses.GeneralTestResult.GeneralTestResult):
         self.Attributes['TestedObjectType'] = 'CMSPixel_Module'
 
     def PopulateResultData(self):
+        self.Canvas.Clear()
         ROOT.gPad.SetLogy(1)
         ROOT.gStyle.SetOptStat(0)
 
-        EfficiencyOverview = self.ParentObject.ResultData['SubTestResults']['EfficiencyOverview_{Rate}'.format(Rate=self.Attributes['Rate'])].ResultData['Plot']['ROOTObject']
+
+        xBins = 8 * self.nCols + 1
+        yBins = 2 * self.nRows + 1
+        EfficiencyOverview = ROOT.TH2D(self.GetUniqueID(), "", xBins, 0., xBins, yBins, 0., yBins)
+
+        # copy ROC data to module data
+        for i in self.ParentObject.ResultData['SubTestResults']['Chips'].ResultData['SubTestResults']:
+            ChipTestResultObject = self.ParentObject.ResultData['SubTestResults']['Chips'].ResultData['SubTestResults'][i]
+            histo = ChipTestResultObject.ResultData['SubTestResults']['EfficiencyMap_{Rate}'.format(Rate=self.Attributes['Rate'])].ResultData['Plot']['ROOTObject']
+            if not histo:
+                print 'cannot get', 'EfficiencyMap_{Rate}'.format(Rate=self.Attributes['Rate']), 'histo for chip ',ChipTestResultObject.Attributes['ChipNo']
+                continue
+            chipNo = ChipTestResultObject.Attributes['ChipNo']
+
+            for col in range(self.nCols):
+                for row in range(self.nRows):
+                    result = histo.GetBinContent(col + 1, row + 1)
+                    self.UpdatePlot(EfficiencyOverview, chipNo, col, row, result)
+
         Ntrig = self.ParentObject.Attributes['Ntrig']['HREfficiency_{Rate}'.format(Rate=self.Attributes['Rate'])]
+#        self.ResultData['Plot']['ROOTObject'] = ROOT.TH1D(self.GetUniqueID(), "", 100, 0, 100.01)
+        self.ResultData['Plot']['ROOTObject'] = ROOT.TH1D(self.GetUniqueID(), "", 50, 0, 100.01)
 
-        self.ResultData['Plot']['ROOTObject'] = ROOT.TH1D(self.GetUniqueID(), "", 100, 0, 100.01)
-
-        for col in range(self.nCols*8): 
+        for col in range(self.nCols*8):
             for row in range(self.nRows*2):
                 result = EfficiencyOverview.GetBinContent(col + 1, row + 1)
                 self.ResultData['Plot']['ROOTObject'].Fill(result*100.0/Ntrig)
@@ -32,3 +51,17 @@ class TestResult(AbstractClasses.GeneralTestResult.GeneralTestResult):
         self.Title = 'Efficiency distribution {Rate}'.format(Rate=self.Attributes['Rate'])
         self.SaveCanvas()
         ROOT.gPad.SetLogy(0)
+
+    def UpdatePlot(self, plot, chipNo, col, row, value):
+        result = value
+        if chipNo < 8:
+            tmpCol = 8 * self.nCols - 1 - chipNo * self.nCols - col
+            tmpRow = 2 * self.nRows - 1 - row
+        else:
+            tmpCol = (chipNo % 8 * self.nCols + col)
+            tmpRow = row
+        # Get the data from the chip sub test result hitmap
+
+        if result and self.verbose:
+            print chipNo, col, row, '--->', tmpCol, tmpRow, result
+        plot.Fill(tmpCol, tmpRow, result)
